6459a91e55ee9555e7e140780ea25c230b3cc77b
Rohan Powar <75206565+MrRohan1777@users.noreply.github.com>
2025-07-03 07:52:39 +0530

    Update Snake2.java

--- Snake2.java
+++ Snake2.java
@@ -9,144 +9,126 @@ public class Snake2 {
 	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Random r=new Random();
-		Scanner sc=new Scanner(System.in);
 
-		int p=0,p2=0;
-		int position=0;
-		int position2=0;
+		Random r = new Random();
+		Scanner sc = new Scanner(System.in);
+
+		int p = 0, p2 = 0;
+		int firstPlayerPosition = 0;
+		int secondPlayerPosition = 0;
+		boolean showDetailsFlag = true;
 		System.out.println("Enter first player name");
-		String player = sc.next();
+		String firstPlayer = sc.next();
 		System.out.println("Enter Second Player Name");
-		String player2=sc.next();
+		String secondPlayer = sc.next();
 		System.out.println("Let's start the game");
-		
-//		Game loop**********
-		
-		while(true) {
-			int dice=r.nextInt(6)+1;
+		String turn = firstPlayer;
 
-			
-		System.out.println("Position Snake at 12,20,34,43,49 number\n");
-		System.out.println("Enter player\n");
-		
-		String round=sc.next();
-//		first player position logic************************
-		if(round.equals(player)) {
-			position+=dice;
-//			first player snake bite************
-			if(position==12||position==20||position==34||position==43||position==48) {
-				System.out.println("Snake bite...........");
-				switch (position) {
-				case 12:
-					position=5;
-					break;
-				case 20:
-					position=14;
-					break;
-				case 34:
-					position=29;
-					break;
-				case 43:
-					position=30;
-					break;
-				case 48:
-					position=15;
-					break;
-					
-				}
-			}
-			
-//			first player laderrr***************
-			
-			else if(position==15||position==28||position==30||position==41) {
-				System.out.println("laderrr.......");
-				switch (position) {
-				case 15:
-					position=25;
-					break;
-				case 28:
-					position=32;
-					break;
-				case 30:
-					position=35;
-					break;
-				case 41:
-					position=46;
-					break;
-					
-				}
-			}
-			
-		}
-		
-//		second player position logic*************8888
-		
-		else if(round.equals(player2)) {
+		// Game loop****
+		while (true) {
+			int dice = r.nextInt(6) + 1;
 
-			position2+=dice;
-			
-//			second player snake bite logic************************************
-			
-			if(position2==12||position2==20||position2==34||position2==43||position2==49) {
-				position2=5;
-				System.out.println("snake bite............\n");
+			if (showDetailsFlag) {
+				System.out.println("Snake Position at 12,20,34,43,49 number");
+				System.out.println("Ladder Position at 15,28,30,41 number\n");
 			}
-			
-//			second player ladderr logic***************************8
-			
-			else if(position2==15||position2==28||position2==30||position2==41) {
-				System.out.println("laderrr.......");
-				switch (position2) {
-				case 15:
-					position2=25;
-					break;
-				case 28:
-					position2=32;
-					break;
-				case 30:
-					position2=35;
-					break;
-				case 41:
-					position2=46;
-					break;
+			System.out.println("Enter player\n");
+			showDetailsFlag = true;
+			String round = sc.next();
+
+			// For first player
+			if (round.equals(firstPlayer) && turn.equals(firstPlayer)) {
+				firstPlayerPosition += dice;
+				snakeBite(firstPlayerPosition);
+				playersLadder(firstPlayerPosition);
+				turn = secondPlayer;
+			}
+			// For Second Player
+			else if (round.equals(secondPlayer) && turn.equals(secondPlayer)) {
+				secondPlayerPosition += dice;
+				snakeBite(secondPlayerPosition);
+				playersLadder(secondPlayerPosition);
+				turn = firstPlayer;
+			} else {
+				System.out.println("Its " + turn + "'s turn.......");
+				showDetailsFlag = false;
+			}
+
+			if ((round.equals(firstPlayer) || round.equals(secondPlayer)) && showDetailsFlag) {
+				System.out.println("Dice is : " + dice);
+				if (firstPlayerPosition < 50) {
+					System.out.println(firstPlayer + "'s position is : " + firstPlayerPosition);
+					p = firstPlayerPosition;
+
+				} else if (firstPlayerPosition > 50) {
+					firstPlayerPosition = p;
+					System.out.println(firstPlayer + "'s position is : " + p);
+				} else if (firstPlayerPosition == 50) {
+					System.out.println(firstPlayer + " win>>>>>>>>>>>>>>>>>>>>>>>>");
+					System.exit(0);
+				}
+
+				if (secondPlayerPosition < 50) {
+					System.out.println(secondPlayer + "'s position is : " + secondPlayerPosition);
+					p2 = secondPlayerPosition;
+
+				} else if (secondPlayerPosition > 50) {
+					secondPlayerPosition = p2;
+					System.out.println(secondPlayer + "'s position is : " + p2);
+				} else if (secondPlayerPosition == 50) {
+					System.out.println(secondPlayer + " win>>>>>>>>>>>>>>>>>>>>>>>>");
+					System.exit(0);
 				}
 			}
-		}else {
-			System.out.println("Enter valid player");
 		}
-				
-		System.out.println("Dice is : "+dice);
-		if(position<50) {
-			System.out.println(player+"'s position is : "+position);
-			 p=position;
-			
-			
-		}else if(position>50) {
-			position=p;
-			System.out.println(player+"'s position is : "+p);
-		}else if(position==50) {
-			System.out.println(player+" win>>>>>>>>>>>>>>>>>>>>>>>>");
-			System.exit(0);;
+
+	}
+
+	private static void playersLadder(int playerPosition) {
+		if (playerPosition == 15 || playerPosition == 28 || playerPosition == 30 || playerPosition == 41) {
+			System.out.println("laderrr.......");
+			switch (playerPosition) {
+			case 15:
+				playerPosition = 25;
+				break;
+			case 28:
+				playerPosition = 32;
+				break;
+			case 30:
+				playerPosition = 35;
+				break;
+			case 41:
+				playerPosition = 46;
+				break;
+
+			}
 		}
-		
-		if(position2<50) {
-			System.out.println(player2+"'s position is : "+position2);
-			 p2=position2;
-			
-			
-		}else if(position2>50) {
-			position2=p2;
-			System.out.println(player2+"'s position is : "+p2);
-		}else if(position2==50) {
-			System.out.println(player2+" win>>>>>>>>>>>>>>>>>>>>>>>>");
-			System.exit(0);
+	}
+
+	private static void snakeBite(int playerPosition) {
+		if (playerPosition == 12 || playerPosition == 20 || playerPosition == 34 || playerPosition == 43
+				|| playerPosition == 49) {
+			System.out.println("Snake bite...........");
+			switch (playerPosition) {
+			case 12:
+				playerPosition = 5;
+				break;
+			case 20:
+				playerPosition = 14;
+				break;
+			case 34:
+				playerPosition = 29;
+				break;
+			case 43:
+				playerPosition = 30;
+				break;
+			case 49:
+				playerPosition = 15;
+				break;
+
+			}
 		}
-		
-		}
-			
+
 			
 	}
 
